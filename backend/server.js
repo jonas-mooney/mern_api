03caab2559549fee90421a85f17d12210f9e6863
Jonas <jonasmooney2@gmail.com>
2023-01-07 14:25:45 -0700

    Goals controller and routes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
 // backend folder will include routes, models, controllers
 
-console.log("Whats poppin jimbo")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
