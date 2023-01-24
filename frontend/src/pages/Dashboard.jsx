9dfe311725a6aa4b145a7cc85bd9d317c87305c5
Jonas <jonasmooney2@gmail.com>
2023-01-24 16:50:35 -0700

    bug fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import React, { Component }  from 'react';
 function Dashboard() {
   return (
     <div>Dashboard</div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import React, { Component }  from 'react';
 import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
